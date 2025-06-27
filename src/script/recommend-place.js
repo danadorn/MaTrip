155512515786555dff6c2becd01c3edca15592e3
Raz <panhawath18@gmail.com>
2025-06-27 21:35:41 +0700

    update to 404 pages

--- src/script/recommend-place.js
+++ src/script/recommend-place.js
@@ -13,7 +13,7 @@ recommendedPlaces.forEach((place, idx) => {
     btn.className = "border-1 broder-gray-300 hover:bg-blue-900 hover:text-white transition duration-500 text-blue-800 text-sm px-4 py-1 rounded-full transition cursor-pointer mb-3";
     btn.setAttribute("data-aos", "fade-up");
     btn.setAttribute("data-aos-delay", 100 + idx * 100); 
-    btn.onclick = () => window.location.href = "../../404.html";
+    btn.onclick = () => window.location.href = "../pages/404.html";
     tagContainer.appendChild(btn);
 });
 
